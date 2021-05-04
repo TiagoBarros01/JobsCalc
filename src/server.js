bd103cc92e72998748224c2155d09cc2f9f84460
tiagobarros01 <tbarros1337@hotmail.com>
2021-05-03 23:04:16 -0300

    feat: created a express server & response

--- src/server.js
+++ src/server.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-useless-return */
 import express from 'express';
 
 const app = express();
+
+app.use('/', (req, res) => {
+  console.log('entered');
+
+  return res.send({ message: 'hello backend' });
+});
+
+app.listen(3000, () => console.log('server is running 🔥'));
